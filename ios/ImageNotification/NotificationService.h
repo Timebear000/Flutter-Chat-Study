e0bc2befd9bd2d11c9a7ca5dfea000d7dd945d03
timebear000 <timebear000@gmail.com>
2021-12-24 17:18:12 +0900

    * Skill Update

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by 김요환 on 2021/12/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
